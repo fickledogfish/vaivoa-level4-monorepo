17edc0de63b043eab068a9213732c3ab47065083
Aníbal Vilela <anibal.vilela@vaivoa.com>
2021-11-17 16:44:46 -0300

    Baixar e listar as petições

--- 07 Whitehouse Petitions/07 Whitehouse Petitions/src/ViewController.swift
+++ 07 Whitehouse Petitions/07 Whitehouse Petitions/src/ViewController.swift
@@ -1,10 +1,18 @@
 import UIKit
 
 class ViewController: UITableViewController {
-    var petitions = [String]()
+    var petitions = [Petition]()
+
+    // let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+    let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        guard let url = URL(string: urlString) else { return }
+        guard let data = try? Data(contentsOf: url) else { return }
+
+        parse(json: data)
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -13,13 +21,23 @@ class ViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let petition = petitions[indexPath.row]
 
         // cell.textLabel?.text = "Title goes here"
         // cell.detailTextLabel?.text = "Subtitle goes here"
 
-        cell.textLabel = "Title goes here"
-        cell.detailTextLabel = "Subtitle goes here"
+        cell.textLabel = petition.title
+        cell.detailTextLabel = petition.body
 
         return cell
+    }
+
+    func parse(json data: Data) {
+        let decoder = JSONDecoder()
+
+        guard let decodedPetitions = try? decoder.decode(Petitions.self, from: data) else { return }
+
+        petitions = decodedPetitions.results
+        tableView.reloadData()
     }
 }
